018fd1eb44f35eb634486d92f452ad8eda248660
SJD <szy@sunzhongyang.com>
2016-10-12 21:03:36 +0800

    init commit

---
+++ HW3/java/com/sunzhongyang/sjd/lab_project_three/MainActivity.java
@@ -0,0 +1,111 @@
+package com.sunzhongyang.sjd.lab_project_three;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+13331233    孙中阳
+2016.10.12  szy@sunzhongyang.com
+ */
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //不在应用中显示应用名
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //载入布局main_layout
+        setContentView(R.layout.main_layout);
+
+        //根据id获取ListView
+        ListView contactList = (ListView) findViewById(R.id.listView);
+
+        //构造一个List用于存储姓名首字母和姓名对
+        final List<Map<String, Object>> data = new ArrayList<>();
+        //构造一个字符串数组用于存储所有姓名
+        final String[] contactNames = new String[] {"Aaron","Elvis","David","Edwin","Frank","Joshua","Ivan","Mark","Joseph","Phoebe"};
+
+        //将姓名首字母和姓名的组合存入List
+        for(int i = 0; i < contactNames.length; i++)
+        {
+            Map<String, Object> tmp = new LinkedHashMap<>();
+            tmp.put("firstC", contactNames[i].charAt(0));
+            tmp.put("contactN", contactNames[i]);
+            data.add(tmp);
+        }
+
+        //构造一个SimpleAdapter,然后使用此Adapter初始化ListView
+        final SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item_layout, new String[] {"firstC", "contactN"}, new int[] {R.id.firstChar, R.id.contactName});
+        contactList.setAdapter(sAdapter);
+
+        //为ListView的每一个Item注册短按事件监听器
+        contactList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int i, long l)
+            {
+                //构造一个Intent,携带被点击Item的姓名的信息创建一个联系人详情页
+                Intent intent = new Intent(MainActivity.this, PageActivity.class);
+                intent.putExtra("key", (String) data.get(i).get("contactN"));
+                startActivity(intent);
+            }
+        });
+
+        ////为ListView的每一个Item注册长按事件监听器
+        contactList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int i, long l)
+            {
+                final int count = i;
+
+                //弹出一个提示框
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("删除联系人")
+                        .setMessage("确定删除联系人" + data.get(count).get("contactN") + "?")
+                        //点击确定按钮则弹框提示确定按钮被按下
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int j)
+                            {
+                                //从数据源删除有关被选中联系人的数据,并通知Adapter有关更改
+                                data.remove(count);
+                                sAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        //点击取消按钮则提示取消按钮被按下
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int j)
+                            {
+
+                            }
+                        })
+                        .create()
+                        .show();
+
+                return true;
+            }
+        });
+    }
+}
---
+++ HW3/java/com/sunzhongyang/sjd/lab_project_three/PageActivity.java
@@ -0,0 +1,147 @@
+package com.sunzhongyang.sjd.lab_project_three;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+13331233    孙中阳
+2016.10.12  szy@sunzhongyang.com
+ */
+
+public class PageActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //不在应用中显示应用名
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //载入布局main_layout
+        setContentView(R.layout.page_layout);
+
+        //得到一个WindowManager以获取当前页面的高度
+        WindowManager wm = this.getWindowManager();
+        int height = wm.getDefaultDisplay().getHeight();
+
+        //构造一个Map以记录联系人姓名机器对应的基本信息
+        Map<String, String[]> contacts = new LinkedHashMap<>();
+
+        //联系人基本信息
+        String[] Aaron	= new String[] {"17715523654", "手机", "江苏苏州电信", "BB4C3B"};
+        String[] Elvis	= new String[] {"18825653224", "手机", "广东揭阳移动", "c48d30"};
+        String[] David	= new String[] {"15052116654", "手机", "江苏无锡移动", "4469b0"};
+        String[] Edwin	= new String[] {"18854211875", "手机", "山东青岛移动", "20A17B"};
+        String[] Frank	= new String[] {"13955188541", "手机", "安徽合肥移动", "BB4C3B"};
+        String[] Joshua	= new String[] {"13621574410", "手机", "江苏苏州移动", "c48d30"};
+        String[] Ivan	= new String[] {"15684122771", "手机", "山东烟台联通", "4469b0"};
+        String[] Mark	= new String[] {"17765213579", "手机", "广东珠海电信", "20A17B"};
+        String[] Joseph	= new String[] {"13315466578", "手机", "河北石家庄电信", "BB4C3B"};
+        String[] Phoebe	= new String[] {"17895466428", "手机", "山东东营移动", "c48d30"};
+
+        //构造Map
+        contacts.put("Aaron", Aaron);
+        contacts.put("Elvis", Elvis);
+        contacts.put("David", David);
+        contacts.put("Edwin", Edwin);
+        contacts.put("Frank", Frank);
+        contacts.put("Joshua", Joshua);
+        contacts.put("Ivan", Ivan);
+        contacts.put("Mark", Mark);
+        contacts.put("Joseph", Joseph);
+        contacts.put("Phoebe", Phoebe);
+
+        //获取自上一页面传来的附加信息
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("key");
+
+        //根据id找到四个TextView
+        TextView nameInLayout = (TextView) findViewById(R.id.name);
+        TextView phone = (TextView) findViewById(R.id.phoneNumber);
+        TextView statu = (TextView) findViewById(R.id.status);
+        TextView pos = (TextView) findViewById(R.id.position);
+
+        //根据选中的联系人初始化各TextView的内容
+        nameInLayout.setText(name);
+        phone.setText(contacts.get(name)[0]);
+        statu.setText(contacts.get(name)[1]);
+        pos.setText(contacts.get(name)[2]);
+
+        //根据id获取返回键和收藏键
+        ImageButton back = (ImageButton) findViewById(R.id.backButton);
+        final ImageButton star = (ImageButton) findViewById(R.id.star);
+
+        //根据id获取最上方带有背景色的相对布局
+        RelativeLayout rel = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        //根据联系人信息设置相对布局的颜色
+        rel.setBackgroundColor(Color.parseColor("#" + contacts.get(name)[3].toUpperCase()));
+
+        //将相对布局的高度设置为当前窗口高度的三分之一
+        rel.getLayoutParams().height = height / 3;
+
+        //获取显示四个选项的ListView
+        ListView checkList = (ListView) findViewById(R.id.checkListView);
+
+        //初始化ListView
+        List<Map<String, String>> data = new ArrayList<>();
+        final String[] options = new String[] {"编辑联系人", "分享联系人", "加入黑名单", "删除联系人"};
+
+        for(int i = 0; i < options.length; i++)
+        {
+            Map<String, String> tmp = new LinkedHashMap<>();
+            tmp.put("option", options[i]);
+            data.add(tmp);
+        }
+
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.options_layout, new String[] {"option"}, new int[] {R.id.op});
+        checkList.setAdapter(sAdapter);
+
+        //为返回键设置短按事件监听器
+        back.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //结束当前页面
+                finish();
+            }
+        });
+
+        //为收藏键设置短按事件监听器
+        star.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(star.getTag().equals("empty"))
+                {
+                    //如果之前是非收藏状态,则转为收藏状态
+                    star.setBackgroundResource(R.drawable.full_star);
+                    star.setTag("full");
+                }
+                else
+                {
+                    star.setBackgroundResource(R.drawable.empty_star);
+                    star.setTag("empty");
+                }
+            }
+        });
+    }
+}
